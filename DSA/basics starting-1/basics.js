2753563f1520ef3c1ff43f25b08ea5965d221a64
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2023-05-01 10:35:07 +0530

    Update basics.js

--- DSA/basics starting-1/basics.js
+++ DSA/basics starting-1/basics.js
@@ -184,7 +184,7 @@ function flatten(arr){
     let flattened = []
     for(let i=0;i<arr.length;i++){
      if(Array.isArray(arr[i])){
-      flattened.concat(flatten(arr[i]))   
+     flattened =  flattened.concat(flatten(arr[i]))   
      }else{
       flattened.push(arr[i])   
      }
