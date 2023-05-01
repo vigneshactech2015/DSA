93157b6c29fb88c0da9dc90183fc323b52c60e55
vigneshactech2015 <85957001+vigneshactech2015@users.noreply.github.com>
2023-05-01 10:52:52 +0530

    Update basics.js

--- DSA/basics starting-1/basics.js
+++ DSA/basics starting-1/basics.js
@@ -194,3 +194,37 @@ function flatten(arr){
 
 //using inbuilt 
 arr.flat(Infinity)
+
+
+//Flatten object using recursion
+
+function flattenObject(obj, parent) {
+    const finalObj = {}
+    const generateFlatObjects = (obj, parent) => {
+        for (let key in obj) {
+            const newParent = parent+key
+            const value = obj[key]
+            if(typeof value === 'object') {
+                generateFlatObjects(value, newParent+".")
+            } else {
+                finalObj[newParent] = value
+            }
+        }
+    }
+    generateFlatObjects(obj, parent)
+    return finalObj
+}
+
+const obj = {
+    A: "12",
+    B: 23,
+    C: {
+      P: 23,
+      O: {
+         L: 56
+      },
+      Q: [1, 2]
+     }   
+  };
+
+console.log(flattenObject(obj, ""))
