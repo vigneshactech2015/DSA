706c28709422a77f4a7c61ecd18ea19d92266876
Vignesh <85957001+vigneshactech2015@users.noreply.github.com>
2023-06-28 15:52:40 +0530

    Update basics.js

--- DSA/basics starting-1/basics.js
+++ DSA/basics starting-1/basics.js
@@ -228,3 +228,16 @@ const obj = {
   };
 
 console.log(flattenObject(obj, ""))
+
+var flat = function (arr, n) {
+    if(n === 0) return arr
+    let result = []
+    for(let i=0;i<arr.length;i++){
+        if(Array.isArray(arr[i])){
+            result.push(...flat(arr[i],n-1))
+        }else{
+            result.push(arr[i])
+        }
+    }
+    return result
+};
