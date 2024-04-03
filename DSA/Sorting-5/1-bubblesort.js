2f56c8e23340cee4471d39498999be08cba6f759
Vignesh <85957001+vigneshactech2015@users.noreply.github.com>
2024-04-03 16:00:30 +0530

    Update 1-bubblesort.js

--- DSA/Sorting-5/1-bubblesort.js
+++ DSA/Sorting-5/1-bubblesort.js
@@ -4,14 +4,14 @@
 
 function bubbleSort(arr){
 
-    for(let i=0;i<arr.length;i++){
-        for(let j=0;j<arr.length;j++){
-            //SWAP
-            if(arr[j]>arr[j+1]){
-                let temp=arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp
-            }
+  for(let i=0;i<arr.length;i++){
+    for(let j=0;j<arr.length;j++){
+        //SWAP
+        if(arr[j]>arr[j+1]){
+            let temp=arr[j];
+            arr[j]=arr[j+1];
+            arr[j+1]=temp
+          }
         }
     }
 return arr
